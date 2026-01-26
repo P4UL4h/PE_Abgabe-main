2ebc00d75264a8257f0429dbad063588814a1b74
P4UL4h <paula.grimm@hfg-gmuend.de>
2026-01-26 20:35:04 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+
+	import { slide } from 'svelte/transition';
+	import Pattern1 from '$lib/components/1_PatternPolygonReactive.svelte';
+	import Pattern2 from '$lib/components/2_PatternPolygonReactiveOffsetSize.svelte';
+	import Pattern3 from '$lib/components/Pattern3.svelte';
+
+	let patterns = [
+		{
+			name: 'Squares in the Grid (offset)',
+			component: Pattern1,
+			description: 'A grid of squares divided into two triangles that can get an offset.'
+		},
+		{
+			name: 'Squares in the Grid (offset and size)',
+			component: Pattern2,
+			description: 'A grid of squares divided into two triangles that can get an offset and size.'
+		},
+		{
+			name: 'Paulas Pattern',
+			component: Pattern3,
+			description: 'Bla Bla'
+		},
+	];
+
+	let selectedPattern = $state(0);
+	let SelectedPattern = $derived(patterns[selectedPattern].component);
+</script>
+
+<div class="app-container">
+	<Header />
+	<main class="app-main">
+		<div class="sidebar-left">
+			{#each patterns as pattern, index}
+				<button
+					class="sidebar-left-item"
+					class:selected={selectedPattern === index}
+					onclick={() => (selectedPattern = index)}
+					>{pattern.name}
+					{#if selectedPattern === index}
+						<div transition:slide class="sidebar-left-description">{pattern.description}</div>
+					{/if}
+				</button>
+			{/each}
+		</div>
+
+		<SelectedPattern />
+	</main>
+</div>
